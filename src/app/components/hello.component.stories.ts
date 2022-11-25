f401720649451e5d19240147a33b6a2fb0170c84
Gery Hirschfeld <gerhard.hirschfeld@baloise.ch>
2022-11-25 22:14:12 +0100

    add storybook

---
+++ src/app/components/hello.component.stories.ts
@@ -0,0 +1,17 @@
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { HelloComponent } from './hello.component';
+
+// More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
+export default {
+  title: 'Components/HelloComponent',
+  component: HelloComponent,
+} as Meta;
+
+// More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
+const Template: Story<HelloComponent> = (args: HelloComponent) => ({
+  props: args,
+});
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/angular/writing-stories/args
+Primary.args = {};
--- src/app/components/hello.component.ts
+++ src/app/components/hello.component.ts
@@ -1,17 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hello',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      hello works!
-    </p>
-  `,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  template: ` <p>{{ label }} works!</p> `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HelloComponent {
-
+  /**
+   * Defines the label of the greetings
+   */
+  @Input() label = 'hello';
 }
